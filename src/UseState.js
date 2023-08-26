6ac4a6e4db11c6a0a366006ca2ece2c767def78f
FROSTYLAN <castillo089frosty@gmail.com>
2023-08-26 10:10:14 -0500

    Estados compuestos con useState

--- src/UseState.js
+++ src/UseState.js
@@ -3,33 +3,33 @@ import React from "react";
 const SECURITY_CODE = "paradigma";
 
 function UseState({ name }) {
-  const [value, setValue] = React.useState("");
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
+  const [state, setState] = React.useState({
+    value: "",
+    error: false,
+    loading: false,
+  });
 
-  console.log(value);
+  console.log(state);
 
   React.useEffect(() => {
     console.log("Empezando el efecto");
 
-    if (loading) {
+    if (state.loading) {
       setTimeout(() => {
         console.log("Haciendo la validación");
 
-        if (value === SECURITY_CODE) {
-          setError(false);
+        if (state.value === SECURITY_CODE) {
+          setState({ ...state, error: false, loading: false });
         } else {
-          setError(true);
+          setState({ ...state, error: true, loading: false });
         }
-
-        setLoading(false);
 
         console.log("Terminando la validación");
       }, 3000);
     }
 
     console.log("Terminando el efecto");
-  }, [loading]);
+  }, [state.loading]);
 
   return (
     <div>
@@ -37,19 +37,19 @@ function UseState({ name }) {
 
       <p>Por favor, escribe el código de seguridad.</p>
 
-      {error && !loading && <p>Error: El código es incorrecto.</p>}
-      {loading && <p>Cargando...</p>}
+      {state.error && !state.loading && <p>Error: El código es incorrecto.</p>}
+      {state.loading && <p>Cargando...</p>}
 
       <input
         placeholder="Código de seguridad"
-        value={value}
+        value={state.value}
         onChange={(event) => {
-          setValue(event.target.value);
+          setState({ ...state, value: event.target.value });
         }}
       />
       <button
         onClick={() => {
-          setLoading(true);
+          setState({ ...state, loading: true });
         }}
       >
         Comprobar
