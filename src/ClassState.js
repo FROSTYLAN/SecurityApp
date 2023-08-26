54691467743e9b4378a699ad07998441cc482cec
FROSTYLAN <castillo089frosty@gmail.com>
2023-08-25 19:41:56 -0500

    Estados compuestos con React.Components

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+const SECURITY_CODE = "paradigma";
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      value: "",
       error: false,
       loading: false,
     };
@@ -16,7 +19,11 @@ class ClassState extends React.Component {
       setTimeout(() => {
         console.log("Haciendo la validación");
 
-        this.setState({ loading: false });
+        if (SECURITY_CODE === this.state.value) {
+          this.setState({ error: false, loading: false });
+        } else {
+          this.setState({ error: true, loading: false });
+        }
 
         console.log("Terminando la validación");
       }, 3000);
@@ -30,10 +37,18 @@ class ClassState extends React.Component {
 
         <p>Por favor, escribe el código de seguridad.</p>
 
-        {this.state.error && <p>Error: El código es incorrecto.</p>}
+        {this.state.error && !this.state.loading && (
+          <p>Error: El código es incorrecto.</p>
+        )}
         {this.state.loading && <p>Cargando...</p>}
 
-        <input placeholder="Código de seguridad" />
+        <input
+          placeholder="Código de seguridad"
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value });
+          }}
+        />
         <button onClick={() => this.setState({ loading: true })}>
           Comprobar
         </button>
